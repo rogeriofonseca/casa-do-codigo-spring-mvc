21f5124faea6df47060af5b75519267c09d3e734
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2015-11-23 01:50:40 -0200

    Acrescentando getList() products e prices em Product.

--- src/main/java/org/casadocodigo/loja/controllers/ProductsController.java
+++ src/main/java/org/casadocodigo/loja/controllers/ProductsController.java
@@ -1,11 +1,15 @@
 package org.casadocodigo.loja.controllers;
 
+import java.util.Map;
 import javax.transaction.Transactional;
 import org.casadocodigo.loja.models.Product;
 import org.casadocodigo.loja.daos.ProductDAO;
+import org.casadocodigo.loja.models.BookType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Transactional
@@ -15,11 +19,20 @@ public class ProductsController {
     private ProductDAO productDAO;
     
     @RequestMapping("/produtos/form")
-    public String form(){
-        return "products/form";
+    public ModelAndView form(){
+        ModelAndView modelAndView = new ModelAndView("products/form");
+        modelAndView.addObject("types", BookType.values());
+        return modelAndView;
     }
     
-    @RequestMapping("/produtos")
+    @RequestMapping(value = "/produtos", method = RequestMethod.GET)
+    public ModelAndView list(){
+        ModelAndView modelAndView = new ModelAndView("products/list");
+        modelAndView.addObject("products",productDAO.list());
+        return modelAndView;
+    }
+    
+    @RequestMapping(value = "/produtos", method = RequestMethod.POST)
     public String save(Product product){
         productDAO.save(product);
         return "products/ok";
