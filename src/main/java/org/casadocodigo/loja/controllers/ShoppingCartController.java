ed1165f4d20126aa063fb458c2038553f9dbbb68
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2015-12-14 00:29:32 -0200

    Alteracoes para carrinho de compras e Amazon S3.

---
+++ src/main/java/org/casadocodigo/loja/controllers/ShoppingCartController.java
@@ -0,0 +1,37 @@
+package org.casadocodigo.loja.controllers;
+
+import com.amazonaws.Request;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.casadocodigo.loja.daos.ProductDAO;
+import org.casadocodigo.loja.models.BookType;
+import org.casadocodigo.loja.models.Product;
+import org.casadocodigo.loja.models.ShoppingCart;
+import org.casadocodigo.loja.models.ShoppingItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/shopping")
+public class ShoppingCartController {
+    
+    @Autowired
+    private ProductDAO productDAO;
+    
+    @Autowired
+    private ShoppingCart shoppingCart;
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public ModelAndView add(Integer productId, BookType bookType){
+        ShoppingItem item = createItem(productId, bookType);
+        shoppingCart.add(item);
+        return new ModelAndView("redirect:/produtos");
+    }
+    
+    private ShoppingItem createItem(Integer productId, BookType bookType){
+        Product product = productDAO.find(productId);
+        ShoppingItem item = new ShoppingItem(product,bookType);
+        return item;
+    }
+}
---
+++ src/main/java/org/casadocodigo/loja/models/ShoppingItem.java
@@ -0,0 +1,16 @@
+
+package org.casadocodigo.loja.models;
+
+import java.math.BigDecimal;
+
+public class ShoppingItem {
+
+    public ShoppingItem(Product product, BookType bookType) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    BigDecimal getTotal(Integer quantity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
